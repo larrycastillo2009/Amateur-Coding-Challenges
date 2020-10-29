63013bb0824719ae75d86f43646dd1124160a87a
Larry Castillo <larrycastillo2009@gmail.com>
2020-10-29 17:30:57 -0500

    problem 3 complete

--- index.js
+++ index.js
@@ -26,3 +26,26 @@ console.log(findEnglish("lkajsdfjjsdfenglishjsdkfjasdf"));
 console.log(findEnglish("lkajsdfjjsdfjsdkfjasdf"));
 
 // Problem #3
+
+function posNeg(arr){
+    var counter = 0;
+    var negSum = 0;
+    var newArr = [];
+
+    arr.forEach(function (number) {
+        if(number > 0){
+            counter ++;
+        }else if (number < 0){
+            negSum += number
+        }
+    })
+
+    newArr.push(counter);
+    newArr.push(negSum);
+
+    return newArr;
+}
+
+console.log(posNeg([1,2,3,-3,-4,-5]));
+console.log(posNeg([1,2,3,3,4,5]));
+console.log(posNeg([-1,-2,-3,-3,-4,-5]));
