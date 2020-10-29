8ce8755b234fdf1d7472c64f3bb3d45ca9b8ba30
Larry Castillo <larrycastillo2009@gmail.com>
2020-10-29 17:18:51 -0500

    problem 2 complete

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function numSeq (start,finish,step) {
 
 console.log(numSeq(1,4,1));
 console.log(numSeq(1,10,1));
+
+// Problem #2
+
+function findEnglish(str){
+    var nstr = str.toLowerCase();
+
+    return nstr.includes("english");
+}
+
+console.log(findEnglish("lkajsdfjjsdfenglishjsdkfjasdf"));
+console.log(findEnglish("lkajsdfjjsdfjsdkfjasdf"));
+
+// Problem #3
